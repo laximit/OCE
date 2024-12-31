308e949b31a9aa37b58b637454e8eb595f1c9794
Murilo Lobos <69329984+laximit@users.noreply.github.com>
2024-12-30 21:43:32 -0300

    commit

--- src/components/stories/Stories.tsx
+++ src/components/stories/Stories.tsx
@@ -3,51 +3,54 @@ import { Link } from "react-router-dom"
 export const stories = [
   {
     storyId: "1029_1",
-    title: "Story 1",
+    title: "Sombras do Passado",
     ageRating: "16 Anos",
     tags: [
       "#laximit", 
-      "#ardan", 
-      "#raiya"
+      "#raiya", 
+      "#familia_ravenhart",
+      "#ardan",
+      "#valthorn"
     ],
     storyYear: "1029",
     storyOrder: "1",
     links: [
-      { paragraph: "Lorem ipsum sit amet, consectetur adipiscing elit." },
-      { paragraph: "Vestibulum viverra nisl urna, nec aliquet felis mollis eget." },
-      { paragraph: "Donec ac leo convallis, aliquet velit non, vulputate nisi." }
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
+      { paragraph: "" },
     ],
   },
-  {
-    storyId: "1029_2",
-    title: "Story 2",
-    ageRating: "12 Anos",
-    tags: [
-      "#laximit",
-      "#ardan",
-      "#fubuki"
-    ],
-    storyYear: "1029",
-    storyOrder: "2",
-    links: [
-      { paragraph: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
-      { paragraph: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." }
-    ],
-  },
-  {
-    storyId: "1029_3",
-    title: "Story 3",
-    ageRating: "Livre",
-    tags: [
-      "#ardan",
-      "#fubuki",
-      "#kouji"
-    ],
-    storyYear: "1029",
-    storyOrder: "3",
-    links: [
-      { paragraph: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
-      { paragraph: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." }
-    ],
-  }
+
 ];
